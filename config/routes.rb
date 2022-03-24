75ce8b4048d80b34f875f206daaafa95d426215a
koomekevo <koome.kevo@gurucrafts.net>
2022-03-24 09:26:00 +0300

    Implement reviewer's changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root "users#index"
-
-  get "/users", to: "users#index"
-  get "/users/:id", to: "users#show"
-
-  get "/users/:user_id/posts", to: "user_posts#index"
-  get "/users/:user_id/posts/:id", to: "user_posts#show"
+  resources :users, only: [:index, :show] do
+    resources :posts, only: [:index, :show]
+  end
 end
