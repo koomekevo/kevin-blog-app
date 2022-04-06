84ad119f0a16f7fba9c476275e90576c77a3ad6a
Annah Tomoro <annaht.annah@gmail.com>
2022-04-06 16:09:41 +0300

    Implement requested changes

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,20 +1,18 @@
 class Post < ApplicationRecord
+  belongs_to :user
   has_many :comments
   has_many :likes
+  after_save :update_posts_counter
 
-  belongs_to :author, class_name: 'User'
-
-  validates :title, presence: true, length: { maximum: 250 }
-  validates :comments_counter, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
-  validates :likes_counter, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
-
-  after_create :update_post_counter
-
-  def update_post_counter
-    author.update(posts_counter: author.posts.size)
+  validates :Title, presence: true
+  validates :Title, length: { maximum: 250 }
+  validates :likes_counter, numericality: { greater_than_or_equal_to: 0 }
+  validates :comments_counter, numericality: { greater_than_or_equal_to: 0 }
+  def recent_comments
+    comments.order(created_at: :desc).limit(5)
   end
 
-  def recent_comments
-    comments.last(5)
+  def update_posts_counter
+    user.increment!(:posts_counter)
   end
 end
